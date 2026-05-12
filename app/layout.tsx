fc952bda72c483c071bf23ded6062f6dcc6ef4e1
Kokie Molepo <kokieMolepo3@gmail.com>
2026-05-12 13:47:13 +0200

    Update global styles and layout for improved design and responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,14 @@
-// app/layout.tsx
 import type { ReactNode } from 'react';
+import { Chivo } from 'next/font/google';
 import { DataProvider } from '@/context/DataContext';
 import './globals.css';
+
+const chivo = Chivo({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-chivo',
+  weight: ['300', '400', '700', '800', '900'],
+});
 
 type RootLayoutProps = {
   children: ReactNode;
@@ -9,7 +16,7 @@ type RootLayoutProps = {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="en" className={chivo.variable}>
       <body>
         <DataProvider>
           {children}
